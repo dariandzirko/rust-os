04818e1f1a8af084aa4c7e4f6fb8cefd2f00d603
darian <dariandzirko@gmail.com>
2022-06-24 19:59:14 -0600

    I had to take a break this is just so dense and my plane is about to board. I started creating the lib for proper testing functionality

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,14 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 mod serial;
 
-use core::{panic::PanicInfo, fmt::Write};
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
+use core::panic::PanicInfo;
+use rust_os::println;
 
 //This function is called on panic.
 #[cfg(not(test))]
@@ -35,43 +20,8 @@ fn panic(info: &PanicInfo) -> ! {
 
 #[cfg(test)]
 #[panic_handler]
-fn panic(info: &PanicInfo) ->! {
-    serial_print!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop{}
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where 
-    T:Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-
-    println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+fn panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic_handler(info)
 }
 
 #[no_mangle] // don't mangle the name of this function
